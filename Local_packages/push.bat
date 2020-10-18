3c31775d910fb7465cdba13b82f078ab2a7f1861
Kelly Loum <kelly.loum@gmail.com>
2020-10-18 18:00:32 -0500

    .

--- Local_packages/push.bat
+++ Local_packages/push.bat
@@ -1,2 +1,2 @@
-dotnet nuget push ../src/bin/release/Blotch3D.1.0.1.nupkg --api-key oy2c5c5rnmcaeymmbo6qzrhscbhjolxu3gwz7cs7ylevmy --source https://api.nuget.org/v3/index.json
+dotnet nuget push ../src/bin/release/Blotch3D.1.0.1.nupkg --api-key (key) --source https://api.nuget.org/v3/index.json
 pause
