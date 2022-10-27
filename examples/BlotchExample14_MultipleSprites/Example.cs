0a7e92c85a662301efeb91f4cc95926442a103bf
Kelly Loum <kelly.loum@gmail.com>
2022-10-26 22:45:32 -0400

    added example14

---
+++ examples/BlotchExample14_MultipleSprites/Example.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using System;
+using Blotch;
+
+namespace BlotchExample
+{
+	/// <summary>
+	/// The 3D window. This must inherit from BlWindow3D. See BlWindow3D for details.
+	/// </summary>
+	public class Example : BlWindow3D
+	{
+		/// <summary>
+		/// This will be the torus model we draw in the window
+		/// </summary>
+		BlSprite TopSprite;
+
+		/// <summary>
+		/// This will be the font for the help menu we draw in the window
+		/// </summary>
+		SpriteFont Font;
+
+		/// <summary>
+		/// See BlWindow3D for details.
+		/// </summary>
+		protected override void Setup()
+		{
+			// Any type of content (3D models, fonts, images, etc.) can be converted to an XNB file by downloading and
+			// using the mgcb-editor (see Blotch3D.chm for details). XNB files are then normally added to the project
+			// and loaded as shown here. 'Content', here, is the folder that contains the XNB files or subfolders with
+			// XNB files. We need to create one ContentManager object for each top-level content folder we'll be loading
+			// XNB files from. You can create multiple content managers if content is spread over diverse folders. Some
+			// content can also be loaded in its native format using platform specific code (may not be portable) or
+			// certain Blotch3D/Monogame methods, like BlGraphicsDeviceManager.LoadFromImageFile.
+			var MyContent = new ContentManager(Services, "Content");
+
+			// The font we will use to draw the menu on the screen.
+			// "Arial14" is the pathname to the font file
+			Font = MyContent.Load<SpriteFont>("arial14");
+
+			// The model of the toroid
+			var TorusModel = MyContent.Load<Model>("torus");
+
+            // The sprite we draw in this window
+            TopSprite = new BlSprite(Graphics, "TopSprite");
+
+			var spacing = 10;
+
+			for (int x = -5; x < 6; x++)
+			{
+                for (int y = -5; y < 6; y++)
+                {
+                    for (int z = -5; z < 6; z++)
+                    {
+						var name = $"{x},{y},{z}";
+
+                        var s = new BlSprite(Graphics, name);
+
+						s.Matrix.Translation = new Vector3(x * spacing, y * spacing, z * spacing);
+						s.Text = name;
+						s.TextFont = Font;
+						s.TextColor = Color.Green;
+						s.LODs.Add(TorusModel);
+
+                        TopSprite.Add(s);
+                    }
+                }
+            }
+		}
+
+		/// <summary>
+		/// See BlWindow3D for details.
+		/// </summary>
+		/// <param name="timeInfo">Provides a snapshot of timing values.</param>
+		protected override void FrameDraw(GameTime timeInfo)
+		{
+			// Handle the standard mouse and keystroke functions. (This is very configurable)
+			Graphics.DoDefaultGui();
+
+			//
+			// Draw things here using BlSprite.Draw(), graphics.DrawText(), etc.
+			//
+
+			TopSprite.Draw();
+
+			// handle undrawable characters for the specified font (like the infinity symbol)
+			try
+			{
+				var MyHud = $@"
+Camera controls:
+Dolly  -  Wheel
+Zoom   -  Left-CTRL-wheel
+Truck  -  Left-drag 
+Rotate -  Right-drag
+Pan    -  Left-ALT-left-drag
+Reset  -  Esc
+Fine control  -  Left-Shift
+
+Eye: {Graphics.Eye}
+LookAt: {Graphics.LookAt}
+MaxDistance: {Graphics.MaxCamDistance}
+MinDistance: {Graphics.MinCamDistance}
+ViewAngle: {Graphics.Zoom}
+ModelLod: {TopSprite.LodTarget}
+ModelApparentSize: {TopSprite.ApparentSize}";
+
+				Graphics.DrawText(MyHud, Font, new Vector2(50, 50));
+			}
+			catch { }
+		}
+	}
+}
